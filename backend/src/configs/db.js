4bd96bbde76a97751dc15e22fc5e0c2c6efb38e7
dangcc06 <trandangpc06@gmail.com>
2026-05-24 19:51:38 +0700

    build database với docker

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -5,9 +5,10 @@ require("dotenv").config();
 // a new connection for every API request.
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "cinema_booking",
+database: process.env.DB_NAME || "cinema_booking",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
